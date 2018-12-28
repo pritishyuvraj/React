b205bbc6074013c66b36f515e1e373ce26566825
Pritish Yuvraj <pritish.yuvi@gmail.com>
2018-12-28 00:00:28 -0500

    Aesthetic improvement to Camera Roll

--- camera/android_camera_roll/CameraRollApp.js
+++ camera/android_camera_roll/CameraRollApp.js
@@ -10,6 +10,7 @@ import {
   ActivityIndicator,
   Image
 } from "react-native";
+import GridView from "react-native-super-grid";
 
 class CameraRollApp extends Component<Props> {
   constructor(props) {
@@ -22,6 +23,7 @@ class CameraRollApp extends Component<Props> {
   async componentDidMount() {
     CameraRoll.getPhotos({
       first: 20,
+      groupName: "Camera",
       assetType: "Photos"
     })
       .then(r => {
@@ -33,6 +35,29 @@ class CameraRollApp extends Component<Props> {
   }
   render() {
     if (this.state.photos === null) {
+      console.log("Still Loading pictures.....");
+      return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+    return (
+      <GridView
+        itemDimension={130}
+        items={this.state.photos}
+        style={styles.gridView}
+        renderItem={item => (
+          <View style={[styles.itemContainer]}>
+            {console.log("Image path -> ", item)}
+            {console.log("to display -> ", item.node.image.uri)}
+            <Image
+              style={{ height: 100 }}
+              source={{ uri: item.node.image.uri }}
+            />
+          </View>
+        )}
+      />
+    );
+  }
+  render1() {
+    if (this.state.photos === null) {
       console.log("Its loading....");
       return <ActivityIndicator size="large" color="#0000ff" />;
     }
@@ -41,6 +66,7 @@ class CameraRollApp extends Component<Props> {
       <View style={styles.container}>
         <ScrollView>
           {this.state.photos.map((p, i) => {
+            console.log("View source -> ", p.node.image.uri);
             return (
               <Image
                 key={i}
@@ -64,6 +90,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
+  },
+  gridView: {
+    paddingTop: 10,
+    flex: 1
+  },
+  itemContainer: {
+    justifyContent: "flex-end",
+    padding: 0.5,
+    height: 100
   }
 });
 
